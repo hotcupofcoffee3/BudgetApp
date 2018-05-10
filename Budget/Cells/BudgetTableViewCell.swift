cfc27e435c820330ce44567fee0f64af56686a93
Adam Moore <adammoore@adams-macbook-pro.local>
2018-05-10 14:04:58 -0600

    Initial addition of 'Budget' and 'Transaction' custom cells, although not much is done to them.

---
+++ Budget/Cells/BudgetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BudgetTableViewCell.swift
+//  Budget
+//
+//  Created by Adam Moore on 5/10/18.
+//  Copyright © 2018 Adam Moore. All rights reserved.
+//
+
+import UIKit
+
+class BudgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
