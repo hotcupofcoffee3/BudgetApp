d1e7421c7d855ffe5dceab6682d704bdb073cf51
Adam Moore <adammoore@adams-macbook-pro.local>
2018-07-05 14:43:56 -0600

    Messing around with initial opening screen to transition to main screen.

--- Budget/Controllers/OpeningScreenViewController.swift
+++ Budget/Controllers/OpeningScreenViewController.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class OpeningScreenViewController: UIViewController {
+    
+    @IBAction func goToMain(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "welcomeToMainSegue", sender: self)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
